78f33d15782c9893980cee252abf23283f556366
Sergey <Malahin.s@gmail.com>
2023-12-08 00:09:09 +0300

    Домашка второго семинара

--- Program.cs
+++ Program.cs
@@ -49,10 +49,56 @@
 // --------------------------------------------------
 // Задача 4
 // на вход принимает число, а на выходе выводит все четные числа от 1 до number включительно, разделенные табуляцией
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-for (int count = 2; count <= number; count +=2)
-Console.Write($"{count}\t");
+//Console.Write("Введите число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//for (int count = 2; count <= number; count +=2)
+//Console.Write($"{count}\t");
+// -----------------------------------------------
+// Задача:  Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+// int a = new Random().Next(150,1000);
+// Console.WriteLine(a);
+// if ( a % 168 == 0 )
+//  Console.WriteLine("YES");
+// else
+//  Console.WriteLine("no");
+//---------------------------------------------------
+// Задача: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и 
+// показывает наибольшую цифру числа.
+////Console.Clear();
+//int a = new Random().Next(10,99);
+//Console.WriteLine(a);
+//int n1 = a / 10;
+//int n2 = a % 10;
+//if (n1 > n2)
+ //   Console.WriteLine(n1);
+//else
+//    Console.WriteLine(n2);
+// ---------------------------------------------------
+// Задача Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую.
+//Console.Write("Введите число N: ");
+//int N = Convert.ToInt32(Console.ReadLine());
+//
+//if (N < 10)
+//Console.Write(N);
+
+//else
+//{
+//    while (N > 0)
+//    {
+//       int carrentDid = N % 10;
+//       N/=10;
+//       if (N > 0)
+//    {
+//        Console.Write(carrentDid + ",");
+//    }
+//       else
+//       {
+//         Console.Write(carrentDid);
+//       }}}
+
+
 
 
 
