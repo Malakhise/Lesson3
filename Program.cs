8274afb7ef822b58b81981ed62655e4f4296a858
Sergey <Malahin.s@gmail.com>
2023-12-11 23:54:30 +0300

    урок 4

--- Program.cs
+++ Program.cs
@@ -99,6 +99,53 @@
 //       }}}
 
 
+Задайте массив из N случайных целых чисел (N вводится с
+клавиатуры).
+Найдите количество чисел, которые оканчиваются на 1 и
+делятся нацело на 7.
+Пример
+[1 5 11 21 81 4 0 91 2 3]
+=> 2
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 20);
+}
+
+// функция возвращает True если число простое, иначе False
+bool CheckProst(int x)
+{
+    for (int j = 2; j < x; j++)
+        if (x % j == 0)
+            return false;
+    return true;
+}
+
+
+void ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (CheckProst(array[i]) == true)
+            count ++;
+    Console.WriteLine(count);
+}
+
+Console.Clear();
+Console.Write("Введите число элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine(string.Join(", ", array));
+ReleaseArray(array);
+
+
+
+
+
+
+
 
 
 
