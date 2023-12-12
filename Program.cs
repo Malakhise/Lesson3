fac34bffab75d3366737376bb56423cc41270f2d
Sergey <Malahin.s@gmail.com>
2023-12-12 23:52:11 +0300

    сделал задачу 3

--- Program.cs
+++ Program.cs
@@ -1,20 +1,61 @@
 ﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100.
 // Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
-Console.Clear();
-Console.Write("Введите кол-во элементов: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[n];
-for(int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(1, 100); //[1;100]
-Console.WriteLine(string.Join(", ", array));
-int count = 0;
-for(int i = 0; i < array.Length; i++)
-    {
-       if (array[i] > 19 && array[i] < 90)
-       count++;
-    } 
- Console.WriteLine(count);
+// Console.Clear();
+// Console.Write("Введите кол-во элементов: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[n];
+// for(int i = 0; i < array.Length; i++)
+//    array[i] = new Random().Next(1, 100); //[1;100]
+// Console.WriteLine(string.Join(", ", array));
+// int count = 0;
+// for(int i = 0; i < array.Length; i++)
+//    {
+//       if (array[i] > 19 && array[i] < 90)
+//       count++;
+//    } 
+// Console.WriteLine(count);
+//-----------------------------------------------------------
+// Задайте массив на 10 целых чисел. Напишите программу, 
+// которая определяет количество чётных чисел в массиве.
+// Console.Clear();
+// Console.Write("Введите кол-во элементов: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[n];
+// for(int i = 0; i < array.Length; i++)
+//    array[i] = new Random().Next(); //[без ограничений]
+// Console.WriteLine(string.Join(", ", array));
+// int count = 0;
+// for(int i = 0; i < array.Length; i++)
+//    {
+//       if (array[i] % 2 == 0)
+//       count++;
+//    } 
+//Console.WriteLine(count);
+//-------------------------------------------------------------
+// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. 
+// Найдите разницу между максимальным и минимальным элементов массива.
 
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.PortableExecutable;
+
+double[] array = {3.14, 2.78, 4.54, 70.35};
+double max = array[0];
+double min = array[0];
+double differece = 0;
+for(int i = 0; i < array.Length; i++)
+ {
+      if (array[i] > max)
+      max = array[i];
+   }
+   for (int j = 0; j < array.Length; j++)
+   {
+    if (array[j] < min)
+     min = array[j];
+   }
+differece = max - min;
+  Console.WriteLine(max);
+Console.WriteLine(min);
+Console.WriteLine(differece);
 
 
 
