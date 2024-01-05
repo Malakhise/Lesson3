1b8b7487b1a63acc9ae2e5445ada94771ad17012
Sergey <Malahin.s@gmail.com>
2024-01-05 13:43:47 +0300

    Сделал первую задачу

---
+++ HW5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+using System.Net;
+
+Console.WriteLine("Введи число m");
+int start = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(n);
+Console.WriteLine("Введи число n");
+int end = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(NaturalNumber(start, end));
+
+string NaturalNumber(int start, int end)
+{
+    if ( start == end)
+        return start.ToString();
+   return $"{start} {NaturalNumber(++start, end)}";
+}
+
