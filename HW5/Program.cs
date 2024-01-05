fc74f048b039776ec547e477eac807fe05c43c89
Sergey <Malahin.s@gmail.com>
2024-01-05 15:44:20 +0300

    отредактировал 3 задачу

--- HW5/Program.cs
+++ HW5/Program.cs
@@ -41,12 +41,18 @@
 //----------------------------------------------------------
 //Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
+// Console.Clear();
 // Console.Write("Введите число M: ");
 // int m = Convert.ToInt32(Console.ReadLine());
 
 // Console.Write("Введите число N: ");
 // int n = Convert.ToInt32(Console.ReadLine());
 
+// if (m < 0 || n < 0)
+// {
+//     Console.WriteLine("Введите число больше 0");
+//     return;
+// }
 // AkkermanFunction(m, n);
 
 // void AkkermanFunction(int m, int n)
